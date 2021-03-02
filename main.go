00317fb8d6202d2b45b4e2cc1c265350421102fa
AngelDhakal <anjaldhakal01@gmail.com>
2021-03-02 23:28:50 +0545

    main package

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	exer "github.com/angeldhakal/go-learn/exercises"
+	// dsa "github.com/angeldhakal/go-learn/exercises/dsa"
+	// "github.com/angeldhakal/go-learn/core"
+)
+
+func main() {
+	// core.Logger()
+	exer.Quiz()
+}
