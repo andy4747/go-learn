c0fafb9d285b9b04923a2b12687668d92c911b6c
AngelDhakal <anjaldhakal01@gmail.com>
2021-03-31 14:00:40 +0545

    use of map

--- core/map.go
+++ core/map.go
@@ -1,7 +1,9 @@
 package core
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 // Maps in golang
@@ -21,4 +23,32 @@ func Maps() {
 	// access the absent key
 	_, isPresent := data["c"]
 	fmt.Println(isPresent)
+}
+
+func equal(x,y map[string]int)bool{
+	if len(x) != len(y){
+		return false
+	}
+	for k, xv := range x {
+		if yv, ok := y[k]; !ok || yv != xv {
+			return false
+		}
+	}
+	return true
+}
+
+
+func Dedup(){
+	seen := make(map[string]bool)
+	input := bufio.NewScanner(os.Stdin)
+	for input.Scan() {
+		line := input.Text()
+		if !seen[line] {
+			seen[line] = true
+			fmt.Println(line)
+		}
+	}
+	if err := input.Err(); err != nil {
+		fmt.Fprintf(os.Stderr, "dedup: %v\n", err)
+	}
 }
